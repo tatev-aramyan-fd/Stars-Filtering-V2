087b9cc44f70c9e20f3aba057473f23000824824
tatev-aramyan <tatev.aramyan.fd@gmail.com>
2022-08-13 10:51:28 +0400

    add gitignore,del unnecessary info, change way of getting header, imports, improve funcnames,and others by review file.

--- calculations_validations.py
+++ calculations_validations.py
@@ -1,4 +1,4 @@
-def sort_by_last_value_in_each_el(lst: list[list[float]]) -> list[list[float]]:   # 2d list/ matrix
+def sort_by_last_value_in_each_el(lst: list[list[float]]) -> list[list[float]]:
     n = len(lst)
     for i in range(n):
         for j in range(0, n - i - 1):
@@ -7,24 +7,30 @@ def sort_by_last_value_in_each_el(lst: list[list[float]]) -> list[list[float]]: 
     return lst
 
 
-def calculate_distance_between_two_points(ra_dec1: tuple, ra_dec2: tuple) -> float:
+def calculate_distance_between_two_points(ra_dec1: tuple, ra_dec2: tuple
+                                          ) -> float:
     ra1, dec1 = ra_dec1
     ra2, dec2 = ra_dec2
     distance = (ra1-ra2)**2 + (dec1-dec2)**2
     return distance
 
 
-def sort_n_stars_by_brightness(lst: list, number_of_stars: int) -> list:
+def get_n_stars_by_brightness(lst: list, number_of_stars: int
+                              ) -> list:
     sorted_by_brightness = sort_by_last_value_in_each_el(lst)
     return sorted_by_brightness[:number_of_stars]
 
 
-def is_in_fov(star_ra: float, star_dec: float, given_ra: float, given_dec: float, fov_h: float, fov_v: float):
+def is_in_fov(
+            star_ra: float, star_dec: float,
+            given_ra: float, given_dec: float,
+            fov_h: float, fov_v: float
+                ):
     h_min = given_ra - fov_h/2
     h_max = given_ra + fov_h/2
     v_min = given_dec - fov_v/2
     v_max = given_dec + fov_v/2
-    if not (is_valid_points(h_min, v_min) and is_valid_points(h_max, v_max)):
+    if not (is_valid_border(h_min, v_min) and is_valid_border(h_max, v_max)):
         raise ValueError("FOV_min and FOV_max are INVALID")
     if h_min <= star_ra <= h_max\
             and v_min <= star_dec <= v_max:
@@ -38,18 +44,25 @@ def is_valid_number(num: int):
     return False
 
 
-def is_valid_points(x: float, y: float):
-    # try:
-    if 0 <= x <= 360 \
-            and -90 <= y <= 90:
+def is_valid_border(x: float, y: float):
+    if 0 <= x <= 360 and -90 <= y <= 90:
         return True
-
     return False
-    # except ValueError:
-    #     raise ValueError("Convert Error")
+
+
+def is_valid_ra(ra: float):
+    if 0 <= ra <= 360:
+        return True
+    return False
+
+
+def is_valid_dec(dec: float):
+    if -90 <= dec <= 90:
+        return True
+    return False
+
 
 def is_valid_fov(h: float, v: float):
-    if 0 <= h <= 360 \
-            and 0 <= v <= 180:
+    if 0 <= h <= 360 and 0 <= v <= 180:
         return True
     return False
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-from data_gather_manage_functions import *
+import calculations_validations as clc_vld
+import data_gather_manage_functions as dt_fnc
 import configs
 
 
 def get_inputs() -> tuple:
     try:
-        print("BE CAREFUL !!!\nRA is in range [0, 360]\nDEC is in range [-90,90]")
         ra = float(input("Enter the RA: "))
         dec = float(input("Enter the DEC: "))
-        print("H is in range [0, 360]\nV is in range [0,180]")
         fov_h = float(input("Enter the Horizontal fov_h: "))
         fov_v = float(input("Enter the Vertical fov_v: "))
         number_of_stars = int(input("Enter number of stars: "))
     except ValueError:
-        raise ValueError("INPUT Value CANT CONVERT to float") from None
+        raise ValueError("INPUT Values CANT CONVERT PROPERLY") from None
     else:
-        if not is_valid_points(ra, dec):
-            raise ValueError("RA DEC is not valid points.")
-        if not is_valid_fov(fov_h, fov_v):
-            raise ValueError("FOV is not valid. ")
-        if not is_valid_number(number_of_stars):
-            raise ValueError("Number of stars must be positive integer. ")
+        if not clc_vld.is_valid_ra(ra):
+            raise ValueError("RA is not valid point, must be in [0,360].")
+        if not clc_vld.is_valid_dec(dec):
+            raise ValueError("DEC is not valid point. Must be in [-90,90]")
+        if not clc_vld.is_valid_fov(fov_h, fov_v):
+            raise ValueError("FOV is not valid.")
+        if not clc_vld.is_valid_number(number_of_stars):
+            raise ValueError("Number of stars must be positive integer.")
 
         return ra, dec, fov_h, fov_v, number_of_stars
 
@@ -27,19 +28,21 @@ def get_inputs() -> tuple:
 def get_final_data(filename: str) -> list[list]:
     ra, dec, fov_h, fov_v, number_of_stars = get_inputs()
 
-    data = store_data_from_tsv(filename)
-    data_in_fov = store_stars_in_fov(ra, dec, fov_v, fov_h, data)
-    sorted_by_brightness = sort_n_stars_by_brightness(data_in_fov, number_of_stars)
-    final_data = update_data_with_distances(sorted_by_brightness, ra, dec)
-
-    return sort_by_last_value_in_each_el(final_data)
+    data = dt_fnc.read_data_from_tsv(filename)
+    data_in_fov = dt_fnc.store_stars_in_fov(ra, dec, fov_v, fov_h, data)
+    sorted_by_brightness = clc_vld.get_n_stars_by_brightness(data_in_fov,
+                                                             number_of_stars)
+    final_data = dt_fnc.update_data_with_distances(sorted_by_brightness,
+                                                   ra, dec
+                                                   )
+    return clc_vld.sort_by_last_value_in_each_el(final_data)
 
 
 def run_application():
-    # filename = configs.test_file
-    filename = configs.get_filename()
-    header = [*get_header_from_file(filename), 'distance']
-    write_in_file(header, get_final_data(filename))
+    filename = configs.filename
+    header = ['ra', 'dec', 'id', 'brightness', 'distance']
+
+    dt_fnc.write_in_file(header, get_final_data(filename))
 
 
 if __name__ == "__main__":
